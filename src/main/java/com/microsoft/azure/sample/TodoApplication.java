4a7800b22f3cc8880238f37922dbabf27dee954f
Matthew-Dong <matthew.dong@foxmail.com>
2018-08-23 13:23:13 +0800

    test for keyvault

--- src/main/java/com/microsoft/azure/sample/TodoApplication.java
+++ src/main/java/com/microsoft/azure/sample/TodoApplication.java
@@ -5,13 +5,22 @@
  */
 package com.microsoft.azure.sample;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner{
+
+    @Value("${uri}")
+    private String uri;
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
+    }
+
+    public void run(String... args) {
+        System.out.println("PRint uri:" + uri);
     }
 }
